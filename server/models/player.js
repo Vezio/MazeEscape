4ccb1406f63f7f6ca690121022e3f361f36bffd3
Tyler Rimaldi <trrimaldi@gmail.com>
2018-10-29 20:08:17 -0400

    Player Functionality

--- server/models/player.js
+++ server/models/player.js
@@ -2,13 +2,13 @@ function Player(name, thirst, steps, inventory) {
   this.name = name;
   this.thirst = thirst;
   this.steps = steps;
-  this.inventory = inventory; //We are going to need to make this an array
+  // this.inventory.push(this.inventory); //We are going to need to make this an array
 }
 
 let players = [
-    new Player("Tyler", 98, 5, "Hammer, Nail")
-  , new Player("Nicholas", 100, 1, "Axe")
-  , new Player("Vezio", 50, 20, "Paper")
+    new Player("Tyler", 98, 5)
+  , new Player("Nicholas", 100, 1)
+  , new Player("Vezio", 50, 20)
 ];
 
 let exists = (x) => typeof x !== 'undefined'
@@ -17,10 +17,10 @@ let exists = (x) => typeof x !== 'undefined'
 
      exports.list = () => players;
      exports.read = (i) => players[i];
-     exports.create = (name, thirst, steps, inventory) => players.push(new Player(name, thirst, steps, inventory)) - 1;
+     exports.create = (name, thirst, steps) => players.push(new Player(name, thirst, steps)) - 1;
      // exports.update = (i, name, thirst, steps, inventory) =>
      //    exists(players[i]) && exists(players[i][name]) && exists(players[i][thirst]) && exists(players[i][name])
      //    && exists(players[i][steps])  && exists(players[i][inventory]) ? players[i][prop] = val : undefined
-     exports.update = (name, atrib, value) =>
+     exports.update = (i, atrib, value) =>
        exists(players[i]) && exists(players[i][atrib]) ? (players[i][atrib] = value) : undefined;
      exports.delete = (i) => exists(i) ? delete players[i] : undefined;
