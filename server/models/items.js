f7732a292ce4caa20c09c0703ad753f9c964d3da
Tyler Rimaldi <trrimaldi@gmail.com>
2018-10-29 20:07:52 -0400

    Item functionality

--- server/models/items.js
+++ server/models/items.js
@@ -1,22 +1,23 @@
-function Item(name, owner, uses, descript) {
-  this.name    = name;
+function Item(owner, name, uses, descript) {
   this.owner   = owner;
+  this.name    = name;
   this.uses    = uses;
-  this.descrip = descript;
+  this.description = descript;
 }
 
 let items = [
-    new Item("chalk", "/rooms/0", 5, "Leave messages on walls")
-  , new Item("rope",  "/rooms/2", 1, "Tie, swing uses")
-  , new Item("plank", "/rooms/3", 1, "Place down to cross")
+    new Item("/rooms/0", "chalk", 5, "Leave messages on walls")
+  , new Item("/rooms/2", "rope", 1, "Tie, swing uses")
+  , new Item("/rooms/3", "plank", 1, "Place down to cross")
 ];
 
 let exists = (x) => typeof x !== 'undefined'
 
 exports.list = () => items;
 exports.read = (i) => items[i];
-exports.create = (name, owner) => items.push(new Item(name, owner, uses, descript)) - 1;
+exports.create = (owner, name, uses, description) => items.push(new Item(owner, name, uses, description)) - 1;
 
+//, uses, descript
 
 // exports.update = (i, name, owner) =>
 //   exists(items[i]) ? (items[i].name = name) && (items[i].owner = owner) : undefined;
@@ -24,9 +25,6 @@ exports.create = (name, owner) => items.push(new Item(name, owner, uses, descrip
 //Updates Single Attribute
 exports.update = (i, atrib, value) =>
   exists(items[i]) && exists(items[i][atrib]) ? (items[i][atrib] = value) : undefined;
-
-
-
 // exports.update = (i, attrib, value) =>
 //   exists(items[i]) ? (items[i].attrib = value) : undefined;
 
