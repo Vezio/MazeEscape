e37f204ac798faf1895ab7e9b67e0098c80f03f0
Tyler Rimaldi <trrimaldi@gmail.com>
2018-10-30 00:37:55 -0400

    organzing

--- server/models/player.js
+++ server/models/player.js
@@ -13,14 +13,9 @@ let players = [
 
 let exists = (x) => typeof x !== 'undefined'
 
-// We need to see how we want to update the Players
-
      exports.list = () => players;
      exports.read = (i) => players[i];
      exports.create = (name, thirst, steps) => players.push(new Player(name, thirst, steps)) - 1;
-     // exports.update = (i, name, thirst, steps, inventory) =>
-     //    exists(players[i]) && exists(players[i][name]) && exists(players[i][thirst]) && exists(players[i][name])
-     //    && exists(players[i][steps])  && exists(players[i][inventory]) ? players[i][prop] = val : undefined
      exports.update = (i, atrib, value) =>
        exists(players[i]) && exists(players[i][atrib]) ? (players[i][atrib] = value) : undefined;
      exports.delete = (i) => exists(i) ? delete players[i] : undefined;
