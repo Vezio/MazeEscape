6e7aacd69d732743b674b269e83a9784e05f947e
Tyler Rimaldi <trrimaldi@gmail.com>
2018-10-30 03:45:08 -0400

    added comments and formatted code

--- server/models/items.js
+++ server/models/items.js
@@ -13,14 +13,23 @@ let items = [
 
 let exists = (x) => typeof x !== 'undefined'
 
+//List all items in the game
 exports.list = () => items;
+
+//List a specific item in the game
 exports.read = (i) => items[i];
+
+//Create an item
 exports.create = (owner, name, uses, description) => items.push(new Item(owner, name, uses, description)) - 1;
+
 //Updates Single Attribute
 exports.update = (i, atrib, value) =>
   exists(items[i]) && exists(items[i][atrib]) ? (items[i][atrib] = value) : undefined;
+
+//Delete an entire item from the game
 exports.delete = (i) => exists(i) ? delete items[i] : undefined
 
+//Give Item to some player
 exports.take = (i, atrib, value) => {
   let location = parseInt(i)
   console.log(i);
@@ -30,13 +39,14 @@ exports.take = (i, atrib, value) => {
   }
 }
 
+//Utlize the item by updating and expressing how much usage is left
 exports.use = (player, item) => {
-   for (let j = 0; j < items.length; j++){
-     if(items[j].owner === parseInt(player) && items[j].name === item.toString())
-        var loc = j;
+  for (let j = 0; j < items.length; j++){
+    if(items[j].owner === parseInt(player) && items[j].name === item.toString())
+      var loc = j;
    }
-    if(items[loc].uses > 0)
-      items[loc].uses = items[loc].uses - 1;
-    else
-      console.log("You are out of uses for this item");
+  if(items[loc].uses > 0)
+    items[loc].uses = items[loc].uses - 1;
+  else
+    console.log("You are out of uses for this item");
 }
