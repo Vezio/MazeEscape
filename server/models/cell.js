d615a99b170131f771bcabd123e133d2e6743cd1
Tyler Rimaldi <trrimaldi@gmail.com>
2018-10-29 20:02:27 -0400

    Added functionality to cell

--- server/models/cell.js
+++ server/models/cell.js
@@ -1,7 +1,7 @@
 function Cell(x, y, items) {
   this.x     = x;
   this.y     = y;
-  this.items = items;
+  this.items = new Array (items);
   // this.fWall = fWall;
   // this.bWall = bWall;
   // this.lWall = lWall;
@@ -11,13 +11,32 @@ function Cell(x, y, items) {
 }
 
 let cells = [
-    new Cell(0,0, "chalk")
-  , new Cell(1,0, "chalk")
-  , new Cell(2,0, "rope")
+    new Cell(0,0, "Chalk")
+  , new Cell(1,0, "Chalk")
+  , new Cell(2,0, "Rope")
   , new Cell(1,1, "plank")
-  , new Cell(1,2, "rope")
-  , new Cell(2,2, "chalk")
 ];
 
 exports.list = ()  => cells;
 exports.read = (i) => cells[i];
+exports.readItems = (i, name) => cells[i].items;
+exports.addItems = (i, name) => cells[i]["items"].push(name);
+exports.delete = (i, name) =>{
+  for(var j = 0; j < cells[i]["items"].length; j++){
+    if (cells[i]["items"][j].toLowerCase() === name.toLowerCase())
+      cells[i]["items"][j] = null;
+  }
+}
+
+
+
+// delete items[i]["items"].name : undefined
+// exports.readSpecItem = (i,name) => cells[i][name];
+// exports.placeItems = (i, atrib, value) =>
+//
+//
+// exports.update = (i, atrib, value) =>
+//   exists(players[i]) && exists(players[i][atrib]) ? (players[i][atrib] = value) : undefined;
+//
+//
+//   items.push(new Item(name, owner, uses, descript)) - 1;
