ae65dce3c3308d2cdcde482d5e86c75d7c6c43b0
Tyler Rimaldi <trrimaldi@gmail.com>
2018-10-30 00:36:38 -0400

    added movement, obstacles, messages

--- server/models/cell.js
+++ server/models/cell.js
@@ -1,42 +1,30 @@
-function Cell(x, y, items) {
-  this.x     = x;
-  this.y     = y;
-  this.items = new Array (items);
-  // this.fWall = fWall;
-  // this.bWall = bWall;
-  // this.lWall = lWall;
-  // this.rWall = rWall;
-  // this.items = items;
-  // this.obstacles = obstacles
+//Reasoning for message array: users may want to use their chalk to add a message
+function Cell(x, y, fwd, bwd, lft, rht, obstacles, messages, items) {
+  this.x         = x;
+  this.y         = y;
+  this.fwd       = fwd;
+  this.bwd       = bwd;
+  this.lft       = lft;
+  this.rht       = rht;
+  this.obstacles = obstacles
+  this.messages  = new Array(messages);
+  this.items     = new Array (items);
 }
 
 let cells = [
-    new Cell(0,0, "Chalk")
-  , new Cell(1,0, "Chalk")
-  , new Cell(2,0, "Rope")
-  , new Cell(1,1, "plank")
+    new Cell(0,0, false, true, true, true, "Hole", "Think about who you are", "Chalk")
+  , new Cell(1,0, true, true, true, true, "No light", "WHAT HAVE YOU DONE", "Chalk")
+  , new Cell(2,0, true, true, true, true, "Bees", "Lets see if you can hang", "Rope")
+  , new Cell(1,1, true, true, true, true, "Posion", "Only the brave can make accross" ,"plank")
 ];
 
 exports.list = ()  => cells;
 exports.read = (i) => cells[i];
-exports.readItems = (i, name) => cells[i].items;
+exports.readItems = (i) => cells[i].items;
 exports.addItems = (i, name) => cells[i]["items"].push(name);
 exports.delete = (i, name) =>{
-  for(var j = 0; j < cells[i]["items"].length; j++){
+  for(let j = 0; j < cells[i]["items"].length; j++){
     if (cells[i]["items"][j].toLowerCase() === name.toLowerCase())
       cells[i]["items"][j] = null;
   }
 }
-
-
-
-// delete items[i]["items"].name : undefined
-// exports.readSpecItem = (i,name) => cells[i][name];
-// exports.placeItems = (i, atrib, value) =>
-//
-//
-// exports.update = (i, atrib, value) =>
-//   exists(players[i]) && exists(players[i][atrib]) ? (players[i][atrib] = value) : undefined;
-//
-//
-//   items.push(new Item(name, owner, uses, descript)) - 1;
