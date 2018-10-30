a6e32a7910c463921d485a10390c08782fdb10cf
Tyler Rimaldi <trrimaldi@gmail.com>
2018-10-30 00:35:41 -0400

    cleaned up

--- server/controllers/gameController.js
+++ server/controllers/gameController.js
@@ -1,10 +1,12 @@
 cell = require("../models/cell.js");
 item = require("../models/items.js");
 
-//Works
+//---------------------Cell-----------------------------------------------------
+
+//List all Cells
 exports.listCells = (req,res) => res.send(cell.list());
 
-//Works
+//Retrieve a specific cell
 exports.getCell = (req,res) => {
   let data = cell.read(req.params.id);
   if (typeof data === 'undefined')
@@ -14,23 +16,13 @@ exports.getCell = (req,res) => {
 };
 
 //List the items in the cell
-exports.seeItems = (req,res) => {
+exports.seeAllCellItems = (req, res) => {
   res.send(cell.readItems(req.params.id))
 }
-
-//Create a new item in the cell
-// exports.createItem = (req,res) => {
-//   console.log(req.body);
-//   if (req.body)
-//     res.status(201).send(item.create(req.body.name, req.body.owner).toString());
-//   else
-//     res.status(400).send("Item name may not be empty.");
-// };
 
 //Create a new item in the cell and add it to the list of items
 exports.addAnItem = (req, res) => {
   if (req.body){
-      // res.status(201).send(item.create(req.params.id, req.body.name, req.params.id).toString());
     if (typeof cell.addItems(req.params.id, req.body.name)
         && typeof item.create(req.params.id, req.body.name, req.body.uses, req.body.description) === 'undefined'){
       res.sendStatus(404);
@@ -42,13 +34,15 @@ exports.addAnItem = (req, res) => {
     res.status(400).send("Item name may not be empty");
 };
 
-//   // cell/id:/take/name
-
-
-//This works, it deletes an item from the cell, but for somereason says not found in body of response
+//Remove an item from a cell. Does not remove the item from the item list
 exports.deleteItem = (req, res) => {
-  if (typeof cell.delete(req.params.id, req.body.name) === 'undefined')
-    res.sendStatus(404);
+  if (cell.delete(req.params.id, req.body.name))
+    res.sendStatus(202)
   else
-    res.sendStatus(204);
+    res.sendStatus(404);
 };
+
+//---------------------Cell-----------------------------------------------------
+
+// TODO: Delete an item from maze and from item list
+//
