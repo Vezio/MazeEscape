13b851e550366eb49253e09a0e7df2f989373fb2
Tyler Rimaldi <trrimaldi@gmail.com>
2018-10-30 00:37:16 -0400

    developing a take method

--- server/models/items.js
+++ server/models/items.js
@@ -6,9 +6,9 @@ function Item(owner, name, uses, descript) {
 }
 
 let items = [
-    new Item("/rooms/0", "chalk", 5, "Leave messages on walls")
-  , new Item("/rooms/2", "rope", 1, "Tie, swing uses")
-  , new Item("/rooms/3", "plank", 1, "Place down to cross")
+    new Item(0, "chalk", 5, "Leave messages on walls")
+  , new Item(2, "rope", 1, "Tie, swing uses")
+  , new Item(3, "plank", 1, "Place down to cross")
 ];
 
 let exists = (x) => typeof x !== 'undefined'
@@ -16,16 +16,18 @@ let exists = (x) => typeof x !== 'undefined'
 exports.list = () => items;
 exports.read = (i) => items[i];
 exports.create = (owner, name, uses, description) => items.push(new Item(owner, name, uses, description)) - 1;
-
-//, uses, descript
-
-// exports.update = (i, name, owner) =>
-//   exists(items[i]) ? (items[i].name = name) && (items[i].owner = owner) : undefined;
-
 //Updates Single Attribute
 exports.update = (i, atrib, value) =>
   exists(items[i]) && exists(items[i][atrib]) ? (items[i][atrib] = value) : undefined;
-// exports.update = (i, attrib, value) =>
-//   exists(items[i]) ? (items[i].attrib = value) : undefined;
-
 exports.delete = (i) => exists(i) ? delete items[i] : undefined
+
+
+
+
+
+// exports.take = (cell,name,newowner) => {
+//   for(let x = 0; x < items.length; x++){
+//     if (items[x].owner === cell && items[x].name === name)
+//       items[x].owner = newowner;
+//   }
+// }
