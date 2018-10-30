f59920f0fdc5cdf904c7392586933ef7a44622c5
Tyler Rimaldi <trrimaldi@gmail.com>
2018-10-30 03:10:28 -0400

    obstacles in progress

--- server/models/obstacles.js
+++ server/models/obstacles.js
@@ -3,3 +3,17 @@
     - Each obstacle has a random spawn time
     - Each obstacle can be defeated
 */
+
+function Obstacle(owner, name, damage, description) {
+  this.owner   = owner;
+  this.name    = name;
+  this.damage    = uses;
+  this.description = description;
+}
+
+let obstacles = [
+    new Obstacle(0, "hole", 100, "Hole of emptyness -- you're dead")
+  , new Obstacle(1, "No light", 0, "Ridicoulessly hard to see")
+  , new Obstacle(2, "Bees", 2, "Stinging Bee")
+  , new Obstacle(3, "Poison", 25, "Poison is in the air")
+];
