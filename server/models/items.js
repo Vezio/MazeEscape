841f93f67af548c02fa20e3f9899621a9ef0828f
Tyler Rimaldi <trrimaldi@gmail.com>
2018-11-03 21:43:08 -0400

    remade take method

--- server/models/items.js
+++ server/models/items.js
@@ -7,6 +7,7 @@ function Item(owner, name, uses, description) {
 
 let items = [
     new Item(0, "chalk", 5, "Leave messages on walls")
+  , new Item(1, "chalk", 5, "Leave messages on walls")
   , new Item(2, "rope", 1, "Tie, swing uses")
   , new Item(3, "plank", 1, "Place down to cross")
 ];
@@ -20,33 +21,41 @@ exports.list = () => items;
 exports.read = (i) => items[i];
 
 //Create an item
-exports.create = (owner, name, uses, description) => items.push(new Item(owner, name, uses, description)) - 1;
+exports.create = (owner, name, uses, description) => items.push(new Item(parseInt(owner), name, uses, description)) - 1;
 
 //Updates Single Attribute
 exports.update = (i, atrib, value) =>
   exists(items[i]) && exists(items[i][atrib]) ? (items[i][atrib] = value) : undefined;
 
 //Delete an entire item from the game
-exports.delete = (i) => exists(i) ? delete items[i] : undefined
+exports.delete = (i) => exists(i) ? delete items[i] : undefined;
 
 //Give Item to some player
-exports.take = (i, atrib, value) => {
-  let location = parseInt(i)
+exports.take = (i, itemName, newOwner) => {
+  let item = itemName.toLowerCase();
+  let updateOwner = newOwner.toString();
+  currentOwner = parseInt(i);
   console.log(i);
+  console.log(item);
+  console.log(updateOwner);
+  // let location = null;
   for(let j = 0; j < items.length; j++){
-    if(items[j][atrib] === (location))
-      items[j][atrib] = value.toString();
+    if(items[j].owner === currentOwner && items[j].name === item)
+        items[j].owner = (updateOwner);
   }
+  // if (typeof location !== null)
+  //     items[j]["owner"] = updateOwner;
 }
 
 //Utlize the item by updating and expressing how much usage is left
 exports.use = (player, item) => {
   for (let j = 0; j < items.length; j++){
-    if(items[j].owner === parseInt(player) && items[j].name === item.toString())
-      var loc = j;
+    if(items[j].owner === parseInt(player) && items[j].name === item.toString()){
+      if(items[j].uses > 0)
+        items[j].uses = items[j].uses - 1;
+      else {
+        console.log(item + " is out of uses");
+      }
+    }
    }
-  if(items[loc].uses > 0)
-    items[loc].uses = items[loc].uses - 1;
-  else
-    console.log("You are out of uses for this item");
 }
