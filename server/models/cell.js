d9f381690989abc16578a0b1c7b483f27911db6e
Tyler Rimaldi <trrimaldi@gmail.com>
2018-11-03 21:42:47 -0400

    remade take method

--- server/models/cell.js
+++ server/models/cell.js
@@ -18,6 +18,9 @@ let cells = [
   , new Cell(3, 1, true, true, true, true, "Poison", "Only the brave can make accross" ,"plank")
 ];
 
+
+let exists = (x) => typeof x !== 'undefined'
+
 //List all cells
 exports.list = ()  => cells;
 
@@ -27,13 +30,18 @@ exports.read = (i) => cells[i];
 //List all items in a specific cell
 exports.readItems = (i) => cells[i].items;
 
+//Delete a cell
+exports.delete = (i) => exists(cells[i]) ? delete cells[i] : undefined;
+
 //Add an item to a cell
 exports.addItems = (i, name) => cells[i]["items"].push(name);
 
 //Remove an item from a cell
-exports.delete = (i, name) =>{
+exports.take = (i, name) =>{
+  let item = name.toLowerCase();
+  // console.log(item);
   for(let j = 0; j < cells[i]["items"].length; j++){
-    if(cells[i]["items"][j].toLowerCase() === name.toLowerCase())
-      cells[i]["items"][j] = null;
+    if(cells[i].items[j].toLowerCase() === item)
+      cells[i]["items"][j] = " ";
   }
 }
