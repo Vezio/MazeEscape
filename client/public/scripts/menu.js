31200965e6e7082f30f087c08fe30a3283d97e47
Tyler Rimaldi <trrimaldi@gmail.com>
2018-11-20 16:34:09 -0500

    edited scrip such that supports new players

--- client/public/scripts/menu.js
+++ client/public/scripts/menu.js
@@ -1,6 +1,8 @@
 
     // Initialize Variables
-    var userName = "";
+    var userName = " ";
+    var playerId = "";
+
     // Menu Selector
     document.body.addEventListener("keyup",function(e){
         switch(e.key ) {
@@ -30,28 +32,50 @@
 
     //Creates a new player based on the user name the user gives at start.
     function newGame() {
+      let player; //all players
+      let found = false;
         userName = prompt("What would you like as your user name?");
         //Check for user name with all spaces
         if (!userName.replace(/\s/g, '').length) {
           alert("Your user name only contained spaces! Please enter a valid user name.");
-          //Check for nulls, if not null then create the player
+          //Check for nulls
         } else if (userName !== null){
-                alert("Good luck to you, " + userName);
-                window.location.href = "maze";
-
-                fetch("http://localhost:3000/api/players", {
-                  method:"POST",
-                  body: '{"name":"'+userName+'"}',
-                  headers: {
-                    "Content-type": "application/json"
-                }})
+            fetch("http://localhost:3000/api/players")
+              .then((res) => res.json())
+              .then(function(json){
+                player = json;
+                console.log(player);
+                //Check if the name is available
+                for(let i = 0; i < player.length; i++){
+                  if (player[i]["name"] === userName.toString()){
+                    found = true;
+                    // console.log(player[i]["name"]);
+                  }
+                }
+                //If the name is available, create the account and pass to maze.
+                if (found === false){
+                  alert("Good luck to you, " + userName);
+                  fetch("http://localhost:3000/api/players", {
+                    method:"POST",
+                    body: '{"name":"'+userName+'"}',
+                    headers: {
+                      "Content-type": "application/json"
+                    }
+                  })
                  .then(function(res){
                    console.log(res.status);
-                });
+
+                   sessionStorage.setItem("playerName", userName);
+                   window.location.href = "maze";
+                  });
+                }else{
+                  alert(userName + ", has been taken already!");
+                }
+              });
           //Feedback for all other uncaught exceptions, if any
-        } else {
-          alert("Please enter a valid user name.")
-        }
+          } else {
+              alert("Please enter a valid user name.");
+            }
     }
 
     function launchSavedGame() {
