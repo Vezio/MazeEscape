db14a63d7cbd594db0cf314038f6738219d77974
Tyler Rimaldi <trrimaldi@gmail.com>
2018-11-20 18:16:32 -0500

    Cleaned up scripts

--- client/public/scripts/maze.js
+++ client/public/scripts/maze.js
@@ -1,143 +1,281 @@
+var playerName = sessionStorage.getItem("playerName"); //player name
+var playerId; //the target player location in the array of players
+var player; //the target player
+var cell;
+var items;
+var inventory;
+var allPlayers; // all players in the game
 
-var player, cell, items, inventory;
-console.log(userName);
 window.onload = function() {
-  fetch("http://localhost:3000/api/players"+userName)
+  fetch("http://localhost:3000/api/players/")
     .then((res) => res.json())
-    .then(function(json){
-      player = json;
-      console.log(player);
-      // Should be a slash after api
-      fetch("http://localhost:3000/api/"+player.loc)
+    .then(function(json) {
+      allPlayers = json;
+      for (let i = 0; i < allPlayers.length; i++) {
+        if (allPlayers[i]["name"] === playerName.toString()) {
+          playerId = i;
+        }
+      }
+      fetch("http://localhost:3000/api/players/" + playerId)
         .then((res) => res.json())
-        .then(loadCell);
-    //   fetch("http://localhost:3000/api/items?owner="+player.loc)
-    //     .then((res) => res.json())
-    //     .then(loadItems);
+        .then(function(json) {
+          player = json;
+          console.log(playerId);
+          fetch("http://localhost:3000/api/" + player.loc)
+            .then((res) => res.json())
+            .then(loadCell);
+        });
     });
-    // fetch("http://localhost:3000/api/items?owner=/actors/"+playerId)
-    // .then((res) => res.json())
-    // .then(loadInventory);
-  }
-
-
-
-//Return to title screen
-function exitGame() {
-    window.location.href = "menu";
 }
-  function loadCell(json) {
-    cell = json;
-    rendercell(cell);
-  }
 
-  function rendercell(cell) {
-    let walls = document.querySelectorAll("main img");
-    switch (player.dir) {
+function loadCell(json) {
+  // console.log("TE")
+  cell = json;
+  renderCell(cell);
+}
+
+function renderCell(cell) {
+  // console.log("TE")
+  let walls = document.querySelectorAll("main img");
+  switch (player.dir) {
     case "north":
-      walls[0].src = cell.west  ? "images/no-wall.png" : "images/wall-left.png";
+      walls[0].src = cell.west ? "images/no-wall.png" : "images/wall-left.png";
       walls[1].src = cell.north ? "images/no-wall.png" : "images/wall-ahead.png";
-      walls[2].src = cell.east  ? "images/no-wall.png" : "images/wall-right.png";
+      walls[2].src = cell.east ? "images/no-wall.png" : "images/wall-right.png";
       break;
     case "south":
-      walls[0].src = cell.east  ? "images/no-wall.png" : "images/wall-left.png";
+      walls[0].src = cell.east ? "images/no-wall.png" : "images/wall-left.png";
       walls[1].src = cell.south ? "images/no-wall.png" : "images/wall-ahead.png";
-      walls[2].src = cell.west  ? "images/no-wall.png" : "images/wall-right.png";
+      walls[2].src = cell.west ? "images/no-wall.png" : "images/wall-right.png";
       break;
     case "east":
       walls[0].src = cell.north ? "images/no-wall.png" : "images/wall-left.png";
-      walls[1].src = cell.east  ? "images/no-wall.png" : "images/wall-ahead.png";
+      walls[1].src = cell.east ? "images/no-wall.png" : "images/wall-ahead.png";
       walls[2].src = cell.south ? "images/no-wall.png" : "images/wall-right.png";
       break;
     case "west":
       walls[0].src = cell.south ? "images/no-wall.png" : "images/wall-left.png";
-      walls[1].src = cell.west  ? "images/no-wall.png" : "images/wall-ahead.png";
+      walls[1].src = cell.west ? "images/no-wall.png" : "images/wall-ahead.png";
       walls[2].src = cell.north ? "images/no-wall.png" : "images/wall-right.png";
       break;
-    }
+  }
+}
+
+function turnLeft() {
+  switch (player.dir) {
+    case "north":
+      player.dir = "west";
+      break;
+    case "west":
+      player.dir = "south";
+      break;
+    case "south":
+      player.dir = "east";
+      break;
+    case "east":
+      player.dir = "north";
+      break;
+  }
+  fetch("http://localhost:3000/api/players/" + playerId, {
+      method: "PATCH",
+      body: '{"attrib":"dir","value":"' + player.dir + '"}',
+      headers: {
+        "Content-type": "application/json"
+      }
+    })
+    .then(function(res) {
+      console.log(res.status);
+    });
+  renderCell(cell);
+}
+
+function turnRight() {
+  switch (player.dir) {
+    case "north":
+      player.dir = "east";
+      break;
+    case "east":
+      player.dir = "south";
+      break;
+    case "south":
+      player.dir = "west";
+      break;
+    case "west":
+      player.dir = "north";
+      break;
+  }
+  fetch("http://localhost:3000/api/players/" + playerId, {
+      method: "PATCH",
+      body: '{"attrib":"dir","value":"' + player.dir + '"}',
+      headers: {
+        "Content-type": "application/json"
+      }
+    })
+    .then(function(res) {
+      console.log(res.status);
+    });
+  renderCell(cell);
+}
+
+function turnAround() {
+  switch (player.dir) {
+    case "north":
+      player.dir = "south";
+      break;
+    case "south":
+      player.dir = "north";
+      break;
+    case "east":
+      player.dir = "west";
+      break;
+    case "west":
+      player.dir = "east";
+      break;
+  }
+  fetch("http://localhost:3000/api/players/" + playerId, {
+      method: "PATCH",
+      body: '{"attrib":"dir","value":"' + player.dir + '"}',
+      headers: {
+        "Content-type": "application/json"
+      }
+    })
+    .then(function(res) {
+      console.log(res.status);
+    });
+  renderCell(cell);
+}
+
+function moveFwd() {
+  switch (player.dir) {
+    case "north":
+      if (cell.north) {
+        console.log(player.loc + "HERE");
+        let newY = cell.y + 1;
+        let currentX = cell.x;
+        player.loc = "/cells/" + currentX + "/" + newY;
+        console.log(player.loc + "HERE");
+      } else
+        alert("There's a wall, you cannot go this way!");
+      break;
+    case "south":
+      if (cell.south) {
+        let newY = cell.y - 1;
+        let currentX = cell.x;
+        player.loc = "/cells/" + currentX + "/" + newY;
+      } else
+        alert("There's a wall, you cannot go this way!");
+      break;
+    case "west":
+      if (cell.west) {
+        // console.log(player.loc + "HERE");
+        let currentY = cell.y;
+        let newX = cell.x - 1;
+        player.loc = "/cells/" + newX + "/" + currentY;
+        console.log(player.loc + "HERE");
+      } else
+        alert("There's a wall, you cannot go this way!");
+      break;
+    case "east":
+      if (cell.east) {
+        // console.log(player.loc + "HERE");
+        let currentY = cell.y;
+        let newX = cell.x + 1;
+        player.loc = "/cells/" + newX + "/" + currentY;
+        console.log(player.loc + "HERE");
+      } else
+        alert("There's a wall, you cannot go this way!");
+      break;
   }
 
+  fetch("http://localhost:3000/api/players/" + playerId, {
+      method: "PATCH",
+      body: '{"attrib":"loc","value":"' + player.loc + '"}',
+      headers: {
+        "Content-type": "application/json"
+      }
+    })
+
+    .then(function(res) {
+      console.log(res.status);
+    });
+
+  fetch("http://localhost:3000/api/" + player.loc)
+    .then((res) => res.json())
+    .then(loadCell);
+
+  renderCell(cell);
+}
+
+
+//Return to title screen
+function exitGame() {
+  window.location.href = "menu";
+}
+
 //Mouse actions: fwd, bwd, lft, rht
-var fwd = document.getElementById("forward").addEventListener("click",function() {
-    forward();
+var fwd = document.getElementById("forward").addEventListener("click", function() {
+  moveFwd();
 });
 
-var bwd = document.getElementById("backward").addEventListener("click",function() {
-    backward();
+var bwd = document.getElementById("backward").addEventListener("click", function() {
+  turnAround();
 });
 
-var lft = document.getElementById("left").addEventListener("click",function() {
-    left();
+var lft = document.getElementById("left").addEventListener("click", function() {
+  turnLeft();
 });
 
-var rht = document.getElementById("right").addEventListener("click",function() {
-    right();
+var rht = document.getElementById("right").addEventListener("click", function() {
+  turnRight();
 });
 
 //Keyboard actions: fwd, bwd, lft, rht
-document.body.addEventListener("keyup",function(e){
-    switch(e.key ) {
-        case "Escape":
-        case " ":
-             exitGame();
-             break;
-        case "ArrowLeft":
-             left();
-             break;
-        case "ArrowUp":
-             forward();
-             break;
-        case "ArrowRight":
-             right();
-             break;
-        case "ArrowDown":
-             backward();
-             break;
-    }
+document.body.addEventListener("keyup", function(e) {
+  switch (e.key) {
+    case "Escape":
+    case " ":
+      exitGame();
+      break;
+    case "ArrowLeft":
+      turnLeft();
+      break;
+    case "ArrowUp":
+      moveFwd();
+      break;
+    case "ArrowRight":
+      turnRight();
+      break;
+    case "ArrowDown":
+      turnAround();
+      break;
+  }
 });
 
 //Use item in game and remove from list of items
 function useItem(e) {
-    var item = e.target;
-    inventory.removeChild(item);
-    console.log("use", item.name);
+  var item = e.target;
+  inventory.removeChild(item);
+  console.log("use", item.name);
 }
 
 //Get the item in inventory that the user requested and pass it to useItem()
 var inventory = document.querySelector("#inventory fieldset");
 var myItems = inventory.querySelectorAll("input");
-    myItems.forEach(function(item) {
-    item.addEventListener("click", useItem);
-    });
+myItems.forEach(function(item) {
+  item.addEventListener("click", useItem);
+});
 
 //Take item from main screen and pass it to the inventory
 function takeItem(e) {
-    var item = e.target;
-    console.log("take", item.name)
-    inventory.appendChild(item);
-    item.addEventListener("click", useItem);
+  var item = e.target;
+  console.log("take", item.name)
+  inventory.appendChild(item);
+  item.addEventListener("click", useItem);
 }
 
 //Select the item on the main screen and pass it to takeItem()
 var mainItem = document.querySelector("main");
 var mainTotItems = mainItem.querySelectorAll("input");
-    mainTotItems.forEach(function(item) {
-    item.addEventListener("click", takeItem);
-    });
-
-//Console messages
-function forward() {
-    console.log("Move forward");
-}
-
-function backward() {
-     console.log("Move backward");
-}
-
-function right() {
-     console.log("Move rightward");
-}
-
-function left() {
-    console.log("Move leftward");
-}
+mainTotItems.forEach(function(item) {
+  item.addEventListener("click", takeItem);
+});
--- client/public/scripts/menu.js
+++ client/public/scripts/menu.js
@@ -1,135 +1,136 @@
-    // Menu Selector
-    document.body.addEventListener("keyup",function(e){
-        switch(e.key ) {
-            case "1":
-                newGame();
-                break;
-            case "2":
-                 launchSavedGame();
-                 break;
-            case "3":
-                 howToPlay();
-                 break;
-            case "4":
-                 leaderboard();
-                 break;
-            case "5":
-                 togglePlay();
-                 break;
-            case "6":
-                 credits();
-                 break;
-        //TODO: Catch user mistakes
-          //default:
-              //alert("An invalid choice has been selected!")
+// Menu Selector
+document.body.addEventListener("keyup", function(e) {
+  switch (e.key) {
+    case "1":
+      newGame();
+      break;
+    case "2":
+      launchSavedGame();
+      break;
+    case "3":
+      howToPlay();
+      break;
+    case "4":
+      leaderboard();
+      break;
+    case "5":
+      togglePlay();
+      break;
+    case "6":
+      credits();
+      break;
+      //TODO: Catch user mistakes
+      //default:
+      //alert("An invalid choice has been selected!")
+  }
+});
+
+//Creates a new player based on the user name the user gives at start.
+function newGame() {
+  let userName = " ";
+  let playerId = "";
+  let player; //all players
+  let found = false;
+  userName = prompt("What would you like as your user name?");
+  //Check for user name with all spaces
+  if (!userName.replace(/\s/g, '').length) {
+    alert("Your user name only contained spaces! Please enter a valid user name.");
+    //Check for nulls
+  } else if (userName !== null) {
+    fetch("http://localhost:3000/api/players")
+      .then((res) => res.json())
+      .then(function(json) {
+        player = json;
+        console.log(player);
+        //Check if the name is available
+        for (let i = 0; i < player.length; i++) {
+          if (player[i]["name"] === userName.toString()) {
+            found = true;
+            // console.log(player[i]["name"]);
+          }
         }
-    });
-
-    //Creates a new player based on the user name the user gives at start.
-    function newGame() {
-      let userName = " ";
-      let playerId = "";
-      let player; //all players
-      let found = false;
-        userName = prompt("What would you like as your user name?");
-        //Check for user name with all spaces
-        if (!userName.replace(/\s/g, '').length) {
-          alert("Your user name only contained spaces! Please enter a valid user name.");
-          //Check for nulls
-        } else if (userName !== null){
-            fetch("http://localhost:3000/api/players")
-              .then((res) => res.json())
-              .then(function(json){
-                player = json;
-                console.log(player);
-                //Check if the name is available
-                for(let i = 0; i < player.length; i++){
-                  if (player[i]["name"] === userName.toString()){
-                    found = true;
-                    // console.log(player[i]["name"]);
-                  }
-                }
-                //If the name is available, create the account and pass to maze.
-                if (found === false){
-                  alert("Good luck to you, " + userName);
-                  fetch("http://localhost:3000/api/players", {
-                    method:"POST",
-                    body: '{"name":"'+userName+'"}',
-                    headers: {
-                      "Content-type": "application/json"
-                    }
-                  })
-                 .then(function(res){
-                   console.log(res.status);
-                   sessionStorage.setItem("playerName", userName);
-                   window.location.href = "maze";
-                  });
-                }else{
-                  alert(userName + ", has been taken already!");
-                }
-              });
-          //Feedback for all other uncaught exceptions, if any
-          } else {
-              alert("Please enter a valid user name.");
-            }
-    }
-
-    function launchSavedGame() {
-      let userName = " ";
-      let playerId = "";
-      let player; //all players
-      let found = false;
-      userName = prompt("Back for more huh? Enter your user name to continue where you left off!");
-      //Check for user name with all spaces
-          fetch("http://localhost:3000/api/players")
-            .then((res) => res.json())
-            .then(function(json){
-              player = json;
-              console.log(player);
-              //Check if the name exists
-              for(let i = 0; i < player.length; i++){
-                if (player[i]["name"] === userName.toString()){
-                  found = true;
-                  // console.log(player[i]["name"]);
-                }
+        //If the name is available, create the account and pass to maze.
+        if (found === false) {
+          alert("Good luck to you, " + userName);
+          fetch("http://localhost:3000/api/players", {
+              method: "POST",
+              body: '{"name":"' + userName + '"}',
+              headers: {
+                "Content-type": "application/json"
               }
-              //If the name is available, create the account and pass to maze.
-              if (found === true){
-                alert("Good luck to you, " + userName);
-                 sessionStorage.setItem("playerName", userName);
-                 window.location.href = "maze";
-              }else{
-                alert("You entered: " + userName + ". Sorry, we couldn't find you. Double check your spelling!");
-              }
+            })
+            .then(function(res) {
+              console.log(res.status);
+              sessionStorage.setItem("playerName", userName);
+              window.location.href = "maze";
             });
-        //Feedback for all other uncaught exceptions, if any
+        } else {
+          alert(userName + ", has been taken already!");
         }
+      });
+    //Feedback for all other uncaught exceptions, if any
+  } else {
+    alert("Please enter a valid user name.");
+  }
+}
 
-    function howToPlay() {
-        window.location.href = "about";
-    }
-
-    function leaderboard() {
-        window.location.href = "leaders";
-    }
-
-    //Grab audio element, if it is clicked and is paused, then play.
-    //If it is clicked and is playing, then pause
-    function togglePlay() {
-        var myAudio = document.getElementById("myAudio");
-        function on() {
-            document.getElementById("Off").style.color="White";
-            document.getElementById("On").style.color="Red";
-            myAudio.play()
+function launchSavedGame() {
+  let userName = " ";
+  let playerId = "";
+  let player; //all players
+  let found = false;
+  userName = prompt("Back for more huh? Enter your user name to continue where you left off!");
+  //Get all players, compare the username the user has provided with the existing players
+  fetch("http://localhost:3000/api/players")
+    .then((res) => res.json())
+    .then(function(json) {
+      player = json;
+      console.log(player);
+      //Check if the name exists
+      for (let i = 0; i < player.length; i++) {
+        if (player[i]["name"] === userName.toString()) {
+          found = true;
+          // console.log(player[i]["name"]);
         }
-        function off() {
-            document.getElementById("On").style.color="White";
-            document.getElementById("Off").style.color="Red";
-            myAudio.pause();
-        }
-        return myAudio.paused ? on() : off();
-    }
+      }
+      //If the name is available, create the account and pass to maze.
+      if (found === true) {
+        alert("Good luck to you, " + userName);
+        sessionStorage.setItem("playerName", userName);
+        window.location.href = "maze";
+      } else {
+        alert("You entered: " + userName + ". Sorry, we couldn't find you. Double check your spelling!");
+      }
+    });
+}
 
-    function credits() {
-        window.location.href = "credits";
-    }
+function howToPlay() {
+  window.location.href = "about";
+}
+
+function leaderboard() {
+  window.location.href = "leaders";
+}
+
+//Grab audio element, if it is clicked and is paused, then play.
+//If it is clicked and is playing, then pause
+function togglePlay() {
+  var myAudio = document.getElementById("myAudio");
+
+  function on() {
+    document.getElementById("Off").style.color = "White";
+    document.getElementById("On").style.color = "Red";
+    myAudio.play()
+  }
+
+  function off() {
+    document.getElementById("On").style.color = "White";
+    document.getElementById("Off").style.color = "Red";
+    myAudio.pause();
+  }
+  return myAudio.paused ? on() : off();
+}
+
+function credits() {
+  window.location.href = "credits";
+}
