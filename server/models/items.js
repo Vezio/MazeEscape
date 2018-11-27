68775b7f11492eb1e1fbd6c66a70412c2e775b1f
Tyler Rimaldi <trrimaldi@gmail.com>
2018-11-26 20:45:05 -0500

    fixed format

--- server/models/items.js
+++ server/models/items.js
@@ -6,10 +6,10 @@ function Item(name, owner, uses, description) {
 }
 
 let items = [
-    new Item("Chalk",   "/cells/0/1", 1, "a piece of chalk")
-  , new Item("Rope",    "/cells/0/0", 1, "a coil of rope")
-  , new Item("Plank",   "/cells/2/0", 1, "a wooden plank")
-  , new Item("Water",   "/cells/2/2", 1, "a canteen")
+   new Item("Chalk", "/cells/0/1", 1, "a piece of chalk")
+  ,new Item("Rope", "/cells/0/0", 1, "a coil of rope")
+  ,new Item("Plank", "/cells/2/0", 1, "a wooden plank")
+  ,new Item("Water", "/cells/2/2", 1, "a canteen")
 ];
 
 
@@ -33,32 +33,36 @@ exports.update = (i, atrib, value) =>
 //Delete an entire item from the game
 exports.delete = (i) => exists(i) ? delete items[i] : undefined;
 
-//Give Item to some player
-exports.take = (i, itemName, newOwner) => {
-  let item = itemName.toLowerCase();
-  let updateOwner = newOwner.toString();
-  currentOwner = parseInt(i);
-  console.log(i);
-  console.log(item);
-  console.log(updateOwner);
-  // let location = null;
-  for(let j = 0; j < items.length; j++){
-    if(items[j].owner === currentOwner && items[j].name === item)
-      items[j].owner = (updateOwner);
-  }
-  // if (typeof location !== null)
-  //     items[j]["owner"] = updateOwner;
-}
+//-------------------------------------------------------------------------------------
+//These are completed in the maze.js but I want to use these here in the future...
 
-//Utlize the item by updating and expressing how much usage is left
-exports.use = (player, item) => {
-  for (let j = 0; j < items.length; j++){
-    if(items[j].owner === player.toString() && items[j].name === item.toString()){
-      if(items[j].uses > 0)
-        items[j].uses = items[j].uses - 1;
-      else {
-        console.log(item + " is out of uses");
-      }
-    }
-   }
-}
+//Give Item to some player
+// exports.take = (i, itemName, newOwner) => {
+//   let item = itemName.toLowerCase();
+//   let updateOwner = newOwner.toString();
+//   currentOwner = parseInt(i);
+//   console.log(i);
+//   console.log(item);
+//   console.log(updateOwner);
+//   // let location = null;
+//   for (let j = 0; j < items.length; j++) {
+//     if (items[j].owner === currentOwner && items[j].name === item)
+//       items[j].owner = (updateOwner);
+//   }
+//   // if (typeof location !== null)
+//   //     items[j]["owner"] = updateOwner;
+// }
+//
+// //Utlize the item by updating and expressing how much usage is left
+// exports.use = (player, item) => {
+//   for (let j = 0; j < items.length; j++) {
+//     if (items[j].owner === player.toString() && items[j].name === item.toString()) {
+//       if (items[j].uses > 0)
+//         items[j].uses = items[j].uses - 1;
+//       else {
+//         console.log(item + " is out of uses");
+//       }
+//     }
+//   }
+// }
+//-------------------------------------------------------------------------------------
