3734c9c70725996e3a292334f372aad541c1153e
Tyler Rimaldi <trrimaldi@gmail.com>
2018-11-26 20:43:20 -0500

    cleaned up game controller

--- server/controllers/gameController.js
+++ server/controllers/gameController.js
@@ -4,7 +4,7 @@ obstacle = require("../models/obstacles.js")
 
 
 //List all Cells
-exports.listCells = (req,res) => res.send(cell.list());
+exports.listCells = (req, res) => res.send(cell.list());
 
 //Retrieve a specific cell
 exports.getCell = (req, res) => {
@@ -14,6 +14,28 @@ exports.getCell = (req, res) => {
   else
     res.send(data);
 };
+
+//Create a new Obstacle in a cell
+exports.createObstacle = (req, res) => {
+  if (req.body) {
+    if (typeof obstacle.addObstacle(req.body.owner, req.body.name, req.body.damage, req.body.onCourse, req.body.icon, req.body.description) === 'undefined') {
+      res.sendStatus(404);
+    } else
+      res.sendStatus(204);
+  } else
+    res.status(400).send("Obstacle information may not be empty");
+};
+
+// Remove a cell --> Need to also add remove all items from the cell in the future
+exports.deleteCell = (req, res) => {
+  if (cell.delete(req.params.x, req.params.y))
+    res.sendStatus(204);
+  else
+    res.sendStatus(404);
+}
+
+//------------------------------------------------------------------------------
+// Don't think these are needed as anymore
 
 // //List the items in the cell
 // exports.seeAllCellItems = (req, res) => {
@@ -33,24 +55,3 @@ exports.getCell = (req, res) => {
 //   else
 //     res.status(400).send("Item information may not be empty");
 // };
-
-//Create a new Obstacle in a cell
-exports.createObstacle = (req, res) => {
-  if (req.body){
-    if(typeof obstacle.addObstacle(req.body.owner, req.body.name, req.body.damage, req.body.onCourse, req.body.icon, req.body.description) === 'undefined'){
-      res.sendStatus(404);
-    }
-    else
-      res.sendStatus(204);
-  }
-  else
-    res.status(400).send("Obstacle information may not be empty");
-};
-
-//Remove a cell --> Need to also add remove all items from the cell in the future
-// exports.deleteCell = (req, res) => {
-//   if (cell.delete(req.params.id))
-//     res.sendStatus(204);
-//   else
-//     res.sendStatus(404);
-// }
