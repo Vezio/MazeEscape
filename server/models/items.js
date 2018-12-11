e9e73dd96e956803ff683810d0f66af3577a55ea
Tyler Rimaldi <trrimaldi@gmail.com>
2018-12-10 22:16:07 -0500

    items to orm, cleaned js, got db working

--- server/models/items.js
+++ server/models/items.js
@@ -1,73 +1,23 @@
-function Item(name, owner, uses, description) {
-  this.name        = name;
-  this.owner       = owner;
-  this.uses        = uses;
-  this.description = description;
-}
+var Sequelize = require("sequelize");
+var sequelize = new Sequelize("database", "username", "password", { dialect: "sqlite", storage: "server/db/items.sqlite" } );
 
-let items = [
-   new Item("Chalk", "/cells/0/1", 1, "a piece of chalk")
-  ,new Item("Rope", "/cells/0/0", 1, "a coil of rope")
-  ,new Item("Plank", "/cells/2/0", 1, "a wooden plank")
-  ,new Item("Water", "/cells/2/2", 1, "a canteen")
-  ,new Item("Anvil", "/cells/0/1", 1, "an anvil for crafting")
-  ,new Item("Hammer", "/cells/0/0", 1, "a hammer for crafting")
-  ,new Item("Workbench", "/cells/2/0", 1, "a work bench for crafting")
-  ,new Item("Metal", "/cells/2/1", 1, "metal ingots for crafting")
-  ,new Item("Key", "EndGame", 1, "the key to end the game")
-];
+var Item = sequelize.define("item", {
+  name: { type: Sequelize.STRING, allowNull: false, unique: true},
+  owner: { type: Sequelize.STRING },
+  uses: { type: Sequelize.INTEGER },
+  description: { type: Sequelize.STRING }
+})
 
+sequelize.sync().then(function() {
+  Item.create({ name: "Chalk",     owner: "/cells/0/1", uses: 1, description: "a piece of chalk"}),
+  Item.create({ name: "Rope",      owner: "/cells/0/1", uses: 1, description: "a piece of chalk"}),
+  Item.create({ name: "Plank",     owner: "/cells/0/1", uses: 1, description: "a piece of chalk"}),
+  Item.create({ name: "Water",     owner: "/cells/2/2", uses: 1, description: "a canteen"}),
+  Item.create({ name: "Anvil",     owner: "/cells/0/1", uses: 1, description: "an anvil for crafting"}),
+  Item.create({ name: "Hammer",    owner: "/cells/0/0", uses: 1, description: "a hammer for crafting"}),
+  Item.create({ name: "Workbench", owner: "/cells/2/0", uses: 1, description: "a work bench for crafting"}),
+  Item.create({ name: "Metal",     owner: "/cells/2/1", uses: 1, description: "metal ingots for crafting"}),
+  Item.create({ name: "Key",       owner: "EndGame",    uses: 1, description: "the key to end the game"})
+})
 
-let exists = (x) => typeof x !== 'undefined'
-
-items.forEach((itm, idx) => itm.id = idx);
-
-//List all items in the game
-exports.list = () => items;
-
-//List a specific item in the game
-exports.read = (i) => items[i];
-
-//Create an item
-exports.create = (name, owner, uses, icon, description) => items.push(new Item(parseInt(owner), name, uses, icon, description)) - 1;
-
-//Updates Single Attribute
-exports.update = (i, atrib, value) =>
-  exists(items[i]) && exists(items[i][atrib]) ? (items[i][atrib] = value) : undefined;
-
-//Delete an entire item from the game
-exports.delete = (i) => exists(i) ? delete items[i] : undefined;
-
-//-------------------------------------------------------------------------------------
-//These are completed in the maze.js but I want to use these here in the future...
-
-//Give Item to some player
-// exports.take = (i, itemName, newOwner) => {
-//   let item = itemName.toLowerCase();
-//   let updateOwner = newOwner.toString();
-//   currentOwner = parseInt(i);
-//   console.log(i);
-//   console.log(item);
-//   console.log(updateOwner);
-//   // let location = null;
-//   for (let j = 0; j < items.length; j++) {
-//     if (items[j].owner === currentOwner && items[j].name === item)
-//       items[j].owner = (updateOwner);
-//   }
-//   // if (typeof location !== null)
-//   //     items[j]["owner"] = updateOwner;
-// }
-//
-// //Utlize the item by updating and expressing how much usage is left
-// exports.use = (player, item) => {
-//   for (let j = 0; j < items.length; j++) {
-//     if (items[j].owner === player.toString() && items[j].name === item.toString()) {
-//       if (items[j].uses > 0)
-//         items[j].uses = items[j].uses - 1;
-//       else {
-//         console.log(item + " is out of uses");
-//       }
-//     }
-//   }
-// }
-//-------------------------------------------------------------------------------------
+module.exports = Item;
--- server/models/player.js
+++ server/models/player.js
@@ -2,65 +2,16 @@ var Sequelize = require("sequelize");
 var sequelize = new Sequelize("database", "username", "password", {dialect: "sqlite", storage: "server/db/players.sqlite"});
 
 var Player = sequelize.define("player", {
-  name: { type: Sequelize.STRING, allowNull: false },
+  name: { type: Sequelize.STRING, allowNull: false, unique: true },
   steps: { type: Sequelize.INTEGER},
   loc: { type: Sequelize.STRING},
   dir: { type: Sequelize.STRING}
 });
 
-
-// sequelize.sync().then(function() {
-//   Player.create({ name: "Tyler",    steps: 0, loc: "/cells/0/1", dir: "north" });
-//   Player.create({ name: "Nicholas", steps: 0, loc: "/cells/0/1", dir: "north" });
-//   Player.create({ name: "Vezio",    steps: 0, loc: "/cells/0/1", dir: "north" });
-//   Player.create({ name: "John",     steps: 0, loc: "/cells/0/1", dir: "north" });
-// });
+sequelize.sync().then(function() {
+  Player.create({ name: "Nicholas", steps: 0, loc: "/cells/0/1", dir: "north" });
+  Player.create({ name: "Vezio",    steps: 0, loc: "/cells/0/1", dir: "north" });
+  Player.create({ name: "John",     steps: 0, loc: "/cells/0/1", dir: "north" });
+});
 
 module.exports = Player;
-
-
-
-
-//
-// function Player(name) {
-//   this.name = name;
-//   this.steps = 0;
-//   this.loc = "/cells/0/1";
-//   this.dir = "north";
-// }
-//
-// let players = [
-//     new Player("Tyler")
-//   , new Player("Nicholas")
-//   , new Player("Vezio")
-//   , new Player("John")
-// ];
-//
-// let exists = (x) => typeof x !== 'undefined'
-//
-// //List all players in the game
-// exports.list = () => players;
-//
-// //List a specific player in the game
-// exports.read = (i) => players[i];
-//
-// //Create a new player
-// exports.create = (name) => players.push(new Player(name)) - 1;
-//
-// //Update a single attribute of the player
-// exports.update = (i, atrib, value) =>{
-//  if (atrib === "steps"){
-//    value = parseInt(players[i].steps +1);
-//  }
-//  exists(players[i]) && exists(players[i][atrib]) ? (players[i][atrib] = value) : undefined;
-// }
-//
-// //Delete an entire player
-// exports.delete = (i) => exists(i) ? delete players[i] : undefined;
-//
-// // exports.read = (name) => {
-// //   for(let i = 0; i<players.length; i++){
-// //     if (players[i].name === name.toString())
-// //       players[i];
-// //   }
-// // }
